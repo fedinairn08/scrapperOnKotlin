cdadd805c1a20d612914baf4898765624621e630
fedinairn08 <fedina_irn08@mail.ru>
2025-07-01 16:36:28 +0300

    Создан проект

---
+++ src/main/kotlin/com/scrapperOnKotlin/ScrapperOnKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.scrapperOnKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScrapperOnKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScrapperOnKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/scrapperOnKotlin/ScrapperOnKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scrapperOnKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScrapperOnKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
