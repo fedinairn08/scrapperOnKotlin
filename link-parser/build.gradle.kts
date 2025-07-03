16ec73143b744bec1b6a7141aff5f1d7990530c2
fedinairn08 <fedina_irn08@mail.ru>
2025-07-03 13:14:26 +0300

    В модуле link-parser написаны unit-тесты с примерами валидных и невалидных ссылок

--- link-parser/build.gradle.kts
+++ link-parser/build.gradle.kts
@@ -33,6 +33,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-web:3.5.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.springframework.boot:spring-boot-starter-test:3.5.0")
 }
 
 kotlin {
--- link-parser/src/test/kotlin/org/linkparser/LinkParserApplicationTests.kt
+++ link-parser/src/test/kotlin/org/linkparser/LinkParserApplicationTests.kt
@@ -1,13 +1,61 @@
 package org.linkparser
 
 import org.junit.jupiter.api.Test
+import org.linkparser.model.GitHubResult
+import org.linkparser.model.StackOverflowResult
+import org.linkparser.service.LinkParser
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.net.URI
+import java.net.URISyntaxException
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 @SpringBootTest
 class LinkParserApplicationTests {
 
+    @Autowired
+    private val linkParser: LinkParser? = null
+
     @Test
-    fun contextLoads() {
+    @Throws(URISyntaxException::class)
+    fun parseValidGithubLink() {
+        val uri = URI("https://github.com/fedinairn08/scrapper")
+
+        val expected = GitHubResult(uri, "fedinairn08", "scrapper")
+        val actual = linkParser!!.parseUrl(uri) as GitHubResult
+
+        assertNotNull(actual)
+        assertEquals(expected.user, actual.user)
+        assertEquals(expected.repo, actual.repo)
     }
 
+    @Test
+    @Throws(URISyntaxException::class)
+    fun parseValidStackOverflowLink() {
+        val uri = URI("https://stackoverflow.com/questions/927358/how-do-i-undo-the-most-recent-local-commits-in-git")
+
+        val expected = StackOverflowResult(uri, 927358L)
+        val actual = linkParser!!.parseUrl(uri) as StackOverflowResult
+
+        assertNotNull(actual)
+        assertEquals(expected.questionId, actual.questionId)
+    }
+
+    @Test
+    @Throws(URISyntaxException::class)
+    fun parseInvalidGithubLink() {
+        val uri = URI("https://github.com/delete")
+
+        assertNull(linkParser!!.parseUrl(uri))
+    }
+
+    @Test
+    @Throws(URISyntaxException::class)
+    fun parseInvalidStackOverflowLink() {
+        val uri = URI("https://stackoverflow.com/")
+
+        assertNull(linkParser!!.parseUrl(uri))
+    }
 }
